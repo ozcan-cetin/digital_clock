8d08ffb0ea68209cf013cf1c22fbfa64e8c3413d
özcan çetin <cetin445@gmail.com>
2022-06-25 22:28:57 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+
+setInterval(
+    function digital_time () {
+        const clock = document.querySelector(".clock");
+        let hour = new Date().getHours();
+        let minute = new Date().getMinutes();
+        let second = new Date().getSeconds();
+
+        hour < 10 ? hour = "0"+ hour : hour;
+        minute < 10 ? minute = "0"+ minute : minute;
+        second < 10 ? second = "0"+ second : second;
+        clock.innerText = `${hour} : ${minute} : ${second}`
+    },1000
+);
