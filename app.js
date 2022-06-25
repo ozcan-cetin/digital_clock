974d91a6a420cc7f7d347d2a70e0300aaf778689
özcan çetin <cetin445@gmail.com>
2022-06-25 22:43:25 +0300

    v3

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 
-setInterval(
-    function digital_time () {
-        const clock = document.querySelector(".clock");
-        let hour = new Date().getHours();
-        let minute = new Date().getMinutes();
-        let second = new Date().getSeconds();
+// setInterval(
+//     function digital_time () {
+//         const clock = document.querySelector(".clock");
+//         let hour = new Date().getHours();
+//         let minute = new Date().getMinutes();
+//         let second = new Date().getSeconds();
 
-        hour < 10 ? hour = "0"+ hour : hour;
-        minute < 10 ? minute = "0"+ minute : minute;
-        second < 10 ? second = "0"+ second : second;
-        clock.innerText = `${hour} : ${minute} : ${second}`
-    },1000
-);
+//         hour < 10 ? hour = "0"+ hour : hour;
+//         minute < 10 ? minute = "0"+ minute : minute;
+//         second < 10 ? second = "0"+ second : second;
+//         clock.innerText = `${hour} : ${minute} : ${second}`
+//     },1000
+// );
+
+//! ************
+setInterval(()=>{
+    let clock=document.querySelector(".clock");
+    let time=new Date().toLocaleTimeString();
+    clock.innerText=time;
+})
